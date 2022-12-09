088a0c5a0adaf96e68a981c634b18a6e705b3df8
CallumDoty <dotycf@vcu.edu>
2022-12-08 19:47:24 -0500

    Update

--- scripts.js
+++ scripts.js
@@ -1,11 +1,11 @@
 function green() {
-    $("body").css("background-color", "green");
-
+    $(".screen").removeClass("active");
+    $(".green").addClass("active");
 }
 
 function blue() {
-    $("body").css("background-color", "blue");
-
+    $(".screen").removeClass("active");
+    $(".blue").addClass("active");
 }
 
 
@@ -20,3 +20,34 @@ function orange() {
     $(".orange").addClass("active");
 }
 
+
+function purple() {
+    $(".screen").removeClass("active");
+    $(".purple").addClass("active");
+}
+
+function yellow() {
+    $(".screen").removeClass("active");
+    $(".yellow").addClass("active");
+}
+
+function brown() {
+    $(".screen").removeClass("active");
+    $(".brown").addClass("active");
+}
+
+function darkgreen() {
+    $(".screen").removeClass("active");
+    $(".darkgreen").addClass("active");
+}
+
+function black() {
+    $(".screen").removeClass("active");
+    $(".black").addClass("active");
+}
+
+
+function limegreen() {
+    $(".screen").removeClass("active");
+    $(".limegreen").addClass("active");
+}
